712f34c1f7d6518f54be255c48a280234b180462
wandl <hiwepy@163.com>
2026-05-27 09:01:13 +0800

    chore(pom): align to Spring Boot 4.0.x (JDK 17, Boot 4.0.1)

--- src/test/java/org/apache/maven/spring/boot/Application.java
+++ src/test/java/org/apache/maven/spring/boot/Application.java
@@ -15,7 +15,7 @@
  */
 package org.apache.maven.spring.boot;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
